9ec09d2fd9edc2d75b08d5c44c8adaa40812d24b
SLV-es <salva@open-office.es>
2023-11-29 12:49:28 +0100

    docs v.1.0.1

--- src/blackjack/usecases/paint-card.js
+++ src/blackjack/usecases/paint-card.js
@@ -1,6 +1,6 @@
 export const paintCard = (carta) => {
   const img = new Image();
-  img.src = `/assets/cartas/${carta}.png`;
+  img.src = `../assets/cartas/${carta}.png`;
   img.className = 'carta';
   img.alt = 'carta ' + carta;
   return img
